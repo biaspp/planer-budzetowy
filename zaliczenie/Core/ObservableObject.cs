22ab61e9f171cb6e541f7ad06c82492b4da49c49
biasp <94643456+Biaasp@users.noreply.github.com>
2023-01-05 21:10:22 +0100

    Dodaj pliki projektów.

---
+++ zaliczenie/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace zaliczenie.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertychanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
